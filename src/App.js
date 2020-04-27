49855573309236e86042ca58c8d9fb0a1dbb03af
mlgmontage <kettleofrassel1@gmail.com>
2020-04-27 08:56:43 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Message from "./components/Message";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
+      <Message />
     </div>
   );
 }
