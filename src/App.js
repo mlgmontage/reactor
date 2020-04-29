52c599110c66c662b814562b41e2dcfac28e62e6
mlgmontage <kettleofrassel1@gmail.com>
2020-04-28 21:17:18 -0700

    cleaning up the greenbox

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from "react";
-import Count from "./components/count";
 
 function App() {
-  return (
-    <div className="App">
-      <Count />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
